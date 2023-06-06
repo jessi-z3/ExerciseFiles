3e8d3b28884ab610952332a662fad953b17ba521
jessi-z3 <jessi_leeann@yahoo.com>
2023-06-06 09:33:05 -0500

    Initial commit

---
+++ Chap04/food.py
@@ -0,0 +1,11 @@
+#ask the user 
+answer = input ("What's my favorite food?")
+
+#result
+if answer == "spaghetti":
+    print ("Yep! So amazing!")
+else:
+    print ("Yuck! That's not it!")
+
+#end message
+print ("Thanks for playing!")
---
+++ Chap05/cities.py
@@ -0,0 +1,7 @@
+#print out the name of a favorite city
+def favorite_city(name):
+   print("One of my favorite cities is", name)
+
+favorite_city("Denton")
+favorite_city("Dallas")
+favorite_city("Orlando")
